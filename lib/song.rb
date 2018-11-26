6f17a7bb4d9c605b5a93c1f6a771cbf97b2e0e60
Cheng Vue <cvu3@ucsc.edu>
2018-11-26 08:46:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,43 +6,55 @@ class Song
   attr_accessor :name, :artist, :genre
 
   def initialize(song_name, artist, genre)
-    @@count += 1
-    @@artists << artist
-    @@genres << genre
     @name = song_name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end
 
   def self.count
-    @@song_counter
+    @@count
   end
 
   def self.genres
-    @@genres
+    unique_genres = []
+    
+    @@genres.each do |genre|
+      unique_genres[genre] == nil ? unique_genres[genre] = 1 : unique_genres[genre] += 1
+    end
+
+    unique_genres
   end
 
   def self.artists
-    @@artists
+    unique_artists = []
+    
+    @@genres.each do |artist|
+      unique_artists[artist] == nil ? unique_artists[artist] = 1 : unique_artists[artist] += 1
+    end
+
+    unique_artists
   end
 
   def self.genre_count
-    @genre_hash = {}
+    genre_hash = {}
 
     @@genres.each do |genre|
       hash[genre] == nil ? hash[genre] = 1 : hash[genre] += 1
     end
 
-    @genre_hash
+    genre_hash
   end
 
   def self.artist_count
-    @artist_hash = {}
+    artist_hash = {}
 
     @@artists.each do |artist|
       hash[artist] == nil ? hash[artisst] = 1 : hash[artist] += 1
     end
 
-    @genre_hash
+    genre_hash
   end
 end
